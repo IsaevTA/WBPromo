419b418e7c6ea16efb0e09877193664b36401f24
Timur Isaev <isaevtimur@mail.ru>
2020-11-27 12:02:47 +0700

    Initial Commit

---
+++ WBPromo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WBPromo
+//
+//  Created by Timur Isaev on 27.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
